0d4b446ebce11c7b986ff1b1a2317f94021dfb3e
Federico Di Natale <federico@MacBook-Air-de-Federico.local>
2022-09-05 15:58:57 +0200

    first push

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProvaComponent } from './prova/prova.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProvaComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
