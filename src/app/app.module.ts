ae0673d24d659348dabc5496a3d9c06f8a13f54b
Federico Di Natale <federico@MacBook-Air-de-Federico.local>
2022-09-07 11:01:21 +0200

    lezione 05

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProvaComponent } from './prova/prova.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// angular design materials
+import { MatSliderModule } from '@angular/material/slider';
+
+// cards material
+import {MatCardModule} from '@angular/material/card';
+
+// button material
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ProvaComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/prova/prova.component.ts
+++ src/app/prova/prova.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-prova',
   templateUrl: './prova.component.html',
   styleUrls: ['./prova.component.css']
 })
-export class ProvaComponent implements OnInit {
+export class ProvaComponent implements OnInit, AfterContentChecked, AfterViewChecked, AfterContentInit, AfterViewInit, DoCheck, OnDestroy {
 
-  constructor() { }
+  constructor() {
+    console.log("contruct");
+
+  }
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked");
+  }
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked");
+  }
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit");
+  }
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit");
+  }
+  ngDoCheck(): void {
+    console.log("ngDoCheck");
+  }
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy");
+  }
 
   ngOnInit(): void {
+    console.log("ngOnInit");
+
   }
 
 }
